86a5559ea8bc9ddf6dba57a0fe8e91ce663b299a
Gamaliel Rodriguez <89487724+Barny-Claus@users.noreply.github.com>
2024-11-22 12:18:02 -0800

    Update validar.php

--- CodigoConexion/validar.php
+++ CodigoConexion/validar.php
@@ -1,16 +1,16 @@
 <?php
-/*este usuario y contraseña son traídos desde el 
+<!-- /*este usuario y contraseña son traídos desde el 
   archivo index y son referentes a los usuarios
   que hay DENTRO de la tabla usuarios en la 
-  base de datos*/
+  base de datos*/-->
 $usuario=$_POST['usuario'];
 $clave=$_POST['contrasena'];
 
 
-/*Por seguridad no se colocarán los valores reales usados, pero 
+<!-- /*Por seguridad no se colocarán los valores reales usados, pero 
   en su lugar se colocará el nombre de qué valor va en dicho 
-  sitio para lograr la conexión*/
-$host="localhost";			//se usa localhost pues se almacenan en el mismo sitio y no en una website distinta o dominio
+  sitio para lograr la conexión*/ -->
+$host="localhost";			<!-- //se usa localhost pues se almacenan en el mismo sitio y no en una website distinta o dominio -->
 $user="UsuarioDeLaBaseDeDatos";
 $pass="ContraseñaDeLaBaseDeDatos";
 $dbname="NombreDeLaBaseDeDatos";
@@ -20,7 +20,7 @@ $conexion=mysqli_connect($host, $user, $pass , $dbname);
 /*en este punto ya estamos conectados y procedemos a hacer la consulta 
 sobre los usuarios DENTRO de la base de datos para tener permisos de uso*/
 
-$consulta="SELECT * FROM usuarios WHERE username='$usuario' and password='$clave'";//las variables capturadas en index
+$consulta="SELECT * FROM usuarios WHERE username='$usuario' and password='$clave'"; <!-- //las variables capturadas en index -->
  //comparativa con los que se encuentran en la base de datos
 $resultado=mysqli_query($conexion, $consulta);
 
